629051e1d42b257b06393b588fe7adca70f2b905
Docile123 <imbereyemasoo@gmail.com>
2026-02-13 14:39:43 +0200

    feat:navbar upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import './App.css'
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <h1>Goo evening</h1>
+   
+    <Navbar/>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav className="flex gap-x-3 items-center justify-center  h-20 shadow-lg shadow-gray-300">
+     <img src="./chefClaudeIcon.png" alt="chef-claude-icon" className="size-12"/>
+     <h1 className="text-xl font-normal sm:text-3xl">Chef Claude</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
